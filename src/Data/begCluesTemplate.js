6dfa9f36fa87f435d3e111161784116b7fab6b44
Tyler Besecker <89785560+TylerBesecker@users.noreply.github.com>
2022-07-06 08:08:48 -0400

    Update begCluesTemplate.js

--- src/Data/begCluesTemplate.js
+++ src/Data/begCluesTemplate.js
@@ -1,7 +1,7 @@
 export const COLLOG = ["Bear_feet","Mole_slippers","Frog_slippers","Demon_feet",
 "Sandwich_lady_hat","Sandwich_lady_top","Sandwich_lady_bottom",
-"Rune_scimitar_ornament_kit_(zamorak)","Rune_scimitar_ornament_kit_(saradomin)","Rune_scimitar_ornament_kit_(guthix)","Monk-s_robe_top_-t-","Monk-s_robe_-t-",
-"Amulet_of_defence_-t","Jester_cape","Shoulder_parrot"]
+"Rune_scimitar_ornament_kit_zamorak-","Rune_scimitar_ornament_kit_saradomin-","Rune_scimitar_ornament_kit_guthix-","Monk-s_robe_top_-t-","Monk-s_robe_-t-",
+"Amulet_of_defence_-t-","Jester_cape","Shoulder_parrot"]
 
 export var ROLLS = 2
 
